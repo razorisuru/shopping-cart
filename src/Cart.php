411bc46d31dfa09020225d2d10a6955c71522d0c
Isuru Bandara <isurubandara318@gmail.com>
2025-01-10 10:00:55 +0530

    user_id function added

--- src/Cart.php
+++ src/Cart.php
@@ -8,9 +8,9 @@ class Cart
 {
     protected $sessionKey = 'shopping_cart';
 
-    public function add($itemId, $quantity, $price, $attributes = [])
+    public function add($userId, $itemId, $quantity, $price, $attributes = [])
     {
-        $cart = session::get($this->sessionKey, []);
+        $cart = session::get($this->getSessionKey($userId), []);
 
         if (isset($cart[$itemId])) {
             $cart[$itemId]['quantity'] += $quantity;
@@ -22,40 +22,51 @@ class Cart
             ];
         }
 
-        session::put($this->sessionKey, $cart);
+        session::put($this->getSessionKey($userId), $cart);
     }
 
-    public function remove($itemId)
+    public function remove($userId, $itemId)
     {
-        $cart = session::get($this->sessionKey, []);
+        $cart = session::get($this->getSessionKey($userId), []);
         unset($cart[$itemId]);
-        session::put($this->sessionKey, $cart);
+        session::put($this->getSessionKey($userId), $cart);
     }
 
-    public function update($itemId, $quantity)
+    public function update($userId, $itemId, $quantity)
     {
-        $cart = session::get($this->sessionKey, []);
+        $cart = session::get($this->getSessionKey($userId), []);
         if (isset($cart[$itemId])) {
             $cart[$itemId]['quantity'] = $quantity;
-            session::put($this->sessionKey, $cart);
+            session::put($this->getSessionKey($userId), $cart);
         }
     }
 
-    public function clear()
+    public function clear($userId)
     {
-        session::forget($this->sessionKey);
+        session::forget($this->getSessionKey($userId));
     }
 
-    public function getAll()
+    public function getAll($userId)
     {
-        return session::get($this->sessionKey, []);
+        return session::get($this->getSessionKey($userId), []);
     }
 
-    public function total()
+    public function total($userId)
     {
-        $cart = session::get($this->sessionKey, []);
+        $cart = session::get($this->getSessionKey($userId), []);
         return array_sum(array_map(function ($item) {
             return $item['quantity'] * $item['price'];
         }, $cart));
+    }
+
+    public function count($userId)
+    {
+        $cart = session::get($this->getSessionKey($userId), []);
+        return array_sum(array_column($cart, 'quantity'));
+    }
+
+    private function getSessionKey($userId)
+    {
+        return "{$this->sessionKey}_user_{$userId}";
     }
 }
